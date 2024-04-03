510e54ff93ba4fea2957b99df5664184f9e89fad
Jean <111933900+Jeanpjy@users.noreply.github.com>
2024-04-03 12:11:59 -0400

    Add Guard & Detector classes

---
+++ Guard.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Guard here.
+ * 
+ * @author Jean
+ * @version Apr 2024
+ */
+public class Guard extends People
+{
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    /**
+     * 
+     */
+    public void detectRobbers(){
+        
+    }
+}
