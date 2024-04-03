1d6a248351af12aa9a47a091ac3d08ed62f9d91c
JerryX2007 <63323160+JerryX2007@users.noreply.github.com>
2024-04-03 11:43:31 -0400

    Adding classes

---
+++ DisplayResults.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class DisplayResults here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DisplayResults extends World
+{
+
+    /**
+     * Constructor for objects of class DisplayResults.
+     * 
+     */
+    public DisplayResults()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
