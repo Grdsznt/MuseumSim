86e8ac26de24c241914b3f11ed2a048ea17a8f8a
DavidX787 <131167198+DavidX787@users.noreply.github.com>
2024-04-04 11:57:20 -0400

    People/Robber Class
    -Created Robber class
    -Created a distanceFrom Method
    -IN progress targetActor method (errors need to resolve)

--- People.java
+++ People.java
@@ -1,4 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Write a description of class People here.
@@ -8,12 +10,45 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public abstract class People extends Actor
 {
-    /**
-     * Act - do whatever the People wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    protected double speed;
+    protected double maxSpeed;
     public void act()
     {
-        // Add your action code here.
+        
+    }
+    
+    public Actor targetActor(Actor act, int dis) 
+    {
+        Actor a;
+        int distance = dis;
+        ArrayList<Actor> targets = (ArrayList<Actor>)getObjectsInRange(dis, Actor.class);
+        double closestTargetDistance = 0;
+        double distanceToActor;
+        Actor closest = null;
+        if(targets.size() > 0) 
+        {
+            closest = targets.get(0);
+            closestTargetDistance = distanceFrom(closest);
+            for(Actor ac: targets) {
+                distanceToActor = distanceFrom(Actor);
+                if(distanceToActor < closestTargetDistance)
+                {
+                    closest = a; 
+                    closestTargetDistance = distanceToActor;
+                }
+                
+            }
+        }
+        return closest;
+    }
+    /**
+     *  Method used to find the distance between two actors 
+     */
+    protected double distanceFrom (Actor act)
+    {
+        double a = Math.abs(getX()-act.getX());
+        double b = Math.abs(getY()-act.getY());
+        double c = Math.hypot(a,b);
+        return c;
     }
 }
