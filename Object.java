2e4ec2ba50d7d8af447e346a99522af3890b04b3
TittoPlayer <161748440+TittoPlayer@users.noreply.github.com>
2024-04-04 11:55:47 -0400

    day 2
    started coding and documenting the valuables and object class

--- Object.java
+++ Object.java
@@ -6,8 +6,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Object extends Actor
+public abstract class Object extends Actor
 {
+    private int hitBoxTop, hitBoxBot, hitBoxLeft, hitBoxRight;
+    public Object(){
+        
+    }
     /**
      * Act - do whatever the Object wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +19,16 @@ public class Object extends Actor
     public void act()
     {
         // Add your action code here.
+    }
+    public void setHitBox(int width, int height){
+        if(this != null){
+            hitBoxTop = getY() - (int)(height/2);
+            hitBoxBot = getY() + (int)(height/2);
+            hitBoxLeft = getX() - (int)(width/2);
+            hitBoxRight = getX() + (int)(width/2);
+        }
+    }
+    public void checkHitBox(int x, int y){
+        
     }
 }
