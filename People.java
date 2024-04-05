5140697292ecf628dddb1c7f726c9cfbb590fc68
DavidX787 <131167198+DavidX787@users.noreply.github.com>
2024-04-05 11:48:19 -0400

    New Changes

--- People.java
+++ People.java
@@ -17,6 +17,10 @@ public class People extends SuperSmoothMover
         
     }
     
+    
+    /**
+     * Method made to target an actor 
+     */
     public Actor targetActor(Actor act, int dis) 
     {
         int distance = dis;
--- Robber.java
+++ Robber.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robber extends People
 {
+    
     public Robber() {
         
     }
