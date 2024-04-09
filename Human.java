a29c546594afbdcd3424b5631dae7c4dceb0a522
Jean <111933900+Jeanpjy@users.noreply.github.com>
2024-04-09 13:56:41 -0400

    Changed People to Human

---
+++ Human.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Human class is a blueprint for all humans that will appear in the world.
+ * 
+ * @author Jean
+ * @version Apr 2024
+ */
+public class Human extends SuperSmoothMover
+{
+    
+    public void act()
+    {
+        
+    }
+}
