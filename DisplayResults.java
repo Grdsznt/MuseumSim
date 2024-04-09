3297108a63503f9784751138e4ac357581fc16e1
DavidX787 <131167198+DavidX787@users.noreply.github.com>
2024-04-09 11:09:22 -0400

    Hi

--- DisplayResults.java
+++ DisplayResults.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class DisplayResults here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * World for Displaying Results at End of the Game
  */
 public class DisplayResults extends World
 {
