1b70811ab00b07a51d37859d54a7b8f1612dcb14
DavidX787 <131167198+DavidX787@users.noreply.github.com>
2024-04-09 12:03:18 -0400

    Working on display results class

--- DisplayResults.java
+++ DisplayResults.java
@@ -5,14 +5,37 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class DisplayResults extends World
 {
-
+    private SuperTextBox endOfGame;
+    private Font comic;
     /**
      * Constructor for objects of class DisplayResults.
      * 
      */
     public DisplayResults()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(500,500,1);
+        
+        comic = new Font("Comic Sans MS", false, false, 16);
+        
+        String[] finalNote = {"GGs... Simulation is over. Here are your Stats"};
+        endOfGame = new SuperTextBox(finalNote, Color.BLACK, Color.WHITE, comic, true, 375, 3, Color.YELLOW);
+        
+        endOfGame.update();
+        int y = getHeight()- endOfGame.getImage().getHeight()/2;
+        int x = endOfGame.getImage().getWidth()/2;
+        addObject(endOfGame, x, y);
     }
+    public void moneyEarned()
+    {
+        
+    }
+    public void theftsPrevented()
+    {
+        
+    }
+    public void furnitureStolen()
+    {
+        
+    }
+    
 }
