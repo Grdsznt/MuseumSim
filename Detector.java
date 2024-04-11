a007e654fb884cb4e3ccaa98be325a7db876a86d
Jean <111933900+Jeanpjy@users.noreply.github.com>
2024-04-11 12:36:55 -0400

    Updated Detector & Guard
    Added Detector’s collision point sets based on its rotation
    Added the function for a Detector to detect Robber
    Added the function for a Guard to catch a Robber

--- Detector.java
+++ Detector.java
@@ -4,6 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * This will be used on objects to detect if they are touching a certain type of class.
  * 
  * //Known bugs:
+ * - Collision points not detecting robbers effectively
  * //Set paint order to put the detector under the guard
  * 
  * @author Jean
@@ -14,7 +15,7 @@ public class Detector extends SuperSmoothMover
     //Set the image
     private GreenfootImage detectorImage = new GreenfootImage("detector.png");
     
-    //Size
+    //Original size
     private int width = detectorImage.getWidth();
     private int height = detectorImage.getHeight();
     
@@ -23,6 +24,9 @@ public class Detector extends SuperSmoothMover
     //The absolute difference of position between this guard & its detector
     private int xDif;
     private int yDif;
+    
+    //Store the robber it detects
+    private Robber robber;
     
     public Detector(Guard guard){
         //Set image
@@ -42,6 +46,7 @@ public class Detector extends SuperSmoothMover
         setLocation(coordinate[0],coordinate[1]);
         //Adjust the Detector's rotation in case it does not match with the Guard's.
         setRotation((360-(guard.getDirection()-1)*90)%360);
+        updateWidthHeight();
     }
     
     /** 
@@ -77,7 +82,80 @@ public class Detector extends SuperSmoothMover
     }
     
     /**
+     * The width & height of the image will change after it is rotated so they should be updated every time after an rotation.
+     */
+    public void updateWidthHeight(){
+        width = getImage().getWidth();
+        height = getImage().getHeight();
+    }
+    
+    /**
+     * The collision points will change after the image is rotated so the collision points should also be updated based on the new width and height of the image.
+     *
+     * @param direction     The direction of the guard & detector to determine which set of collision points to give
+     * @return int[][]   The 2D array containing the x direction & y direction collision points
+     */
+    public int[][] getCollisionPoints(int direction){
+        //Collision points 2D array
+        int[][] collisionPointsRight = {
+                                            {0, width/2, width/2, width, width, width}, 
+                                            {height/2, height/4, 3*height/4, 0, height, height/2}
+                                        };
+        int[][] collisionPointsUp = {
+                                            {width/2, width/4, 3*width/4, 0, width, width/2}, 
+                                            {height, height/2, height/2, 0, 0, 0}
+                                    };
+        int[][] collisionPointsLeft = {
+                                            {width, width/2, width/2, 0, 0, 0}, 
+                                            {height/2, height/4, 3*height/4, 0, height, height/2}
+                                        };
+        int[][] collisionPointsDown = {
+                                            {width/2, width/4, 3*width/4, 0, width, width/2}, 
+                                            {0, height/2, height/2, height, height, height}
+                                        };
+        switch(direction){
+            case 1: return collisionPointsRight;
+            case 2: return collisionPointsUp;
+            case 3: return collisionPointsLeft;
+            default: return collisionPointsDown;
+        }
+    }
+    
+    /**
+     * Detect if this detector detects any robber in range.
+     * 
+     * @return boolean  True if detects a robber, false otherwise.
+     */
+    public boolean detectedRobbers(){
+        boolean detected = false;
+        int[][] collisionPoints = getCollisionPoints(guard.getDirection());
+        for(int i=0; i<collisionPoints[0].length; i++){
+            Robber temp = (Robber)getOneObjectAtOffset(collisionPoints[0][i],collisionPoints[1][i],Robber.class);
+            if(temp!=null){
+                robber = temp;
+                detected = true;
+            }
+        }
+        //Set robber = null if nothing is detected this time
+        if(!detected){
+            robber = null;
+        }
+        return detected;
+    }
+    
+    /**
+     * Get the robber.
+     * 
+     * @return Robber  The robber it detects
+     */
+    public Robber getRobber(){
+        return robber;
+    }
+    
+    /**
      * Get the width of the detector.
+     * 
+     * @return int  width of the current image
      */
     public int getWidth(){
         return width;
@@ -85,6 +163,8 @@ public class Detector extends SuperSmoothMover
     
     /**
      * Get the height of the detector.
+     * 
+     * @return int  height of the current image
      */
     public int getHeight(){
         return height;
