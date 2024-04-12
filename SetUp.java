687db204c62e4db397582c1daa119dc469b6f54e
iUwUc <langchen.felix@gmail.com>
2024-04-12 11:35:57 -0400

    asdas

--- SetUp.java
+++ SetUp.java
@@ -8,30 +8,80 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SetUp extends World
 {
-    private GreenfootImage background; 
+     // instance variables - replace the example below with your own
+    private int counter = 0;
+    private MouseInfo m;
+    private Circle[] population, patient_zeros, masked, vaccinated;
     
-    
-    // Money 
-    private static int startingMoney = 100;
-    private SettingsButton startingMoneyU;
-    private SettingsButton startingMoneyD;
-    
-    //Guards
-    
-    private static int Guards = 10;
-    private SettingsButton GuardsU;
-    private SettingsButton GuardsD;
-
+    private int last_po = 0, last_pa = 0, last_ma = 0, last_va = 0; // keep track of the last circle clicked
+    private int populationCount = 10, patient_zerosCount = 1, maskedCount = 0, vaccinatedCount = 0; // keep track of the values being selected
     /**
-     * Constructor for objects of class SetUp.
-     * 
+     * Constructor for objects of class StartingWorld
      */
     public SetUp()
-    {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
-        if(Greenfoot.mouseClicked(startingMoneyU){
-            set(0);
+    {
+        super(1280, 700, 1);
+        population = new Circle[4];
+        patient_zeros = new Circle[4];
+        masked = new Circle[4];
+        vaccinated = new Circle[4];
+        // Uses arrays to set up circles/options on the starting screen
+        for (int i=0; i<4; i++){
+            population[i] = new Circle(331 + 77 * i, 387, 62, Color.BLUE, (i == 0 ? true : false));
+            masked[i] = new Circle(331 + 77 * i, 550, 62, Color.BLUE, (i == 0 ? true : false));
+        }
+        for (int i=0; i<4; i++){
+            patient_zeros[i] = new Circle(707 + 77 * i, 390, 62, Color.BLUE, (i == 0 ? true : false));
+        }
+        for (int i=0; i<4; i++){
+            vaccinated[i] = new Circle(710 + 77 * i, 557, 62, Color.BLUE, (i == 0 ? true : false));
+        }
+    }
+    
+    public void act(){
+        m = Greenfoot.getMouseInfo();
+        if (m != null){
+            for (int i=0; i<4; i++){
+                // if clicked on a population toggle update corresponding values
+                if (Greenfoot.mouseClicked(population[i])){
+                    population[last_po].setvisible(false);
+                    population[i].setvisible(true);
+                    last_po = i;
+                    populationCount = 10 + 10 * i;
+                }
+                // if clicked on a patient zero toggle update corresponding values
+                if (Greenfoot.mouseClicked(patient_zeros[i])){
+                    patient_zeros[last_pa].setvisible(false);
+                    patient_zeros[i].setvisible(true);
+                    last_pa = i;
+                    patient_zerosCount = 1 + i;
+                }
+                // if clicked on a masked toggle update corresponding values
+                if (Greenfoot.mouseClicked(masked[i])){
+                    masked[last_ma].setvisible(false);
+                    masked[i].setvisible(true);
+                    last_ma = i;
+                    maskedCount = 333 * i;
+                }
+                // if clicked on a vaccinated toggle update corresponding values
+                if (Greenfoot.mouseClicked(vaccinated[i])){
+                    vaccinated[last_va].setvisible(false);
+                    vaccinated[i].setvisible(true);
+                    last_va = i;
+                    vaccinatedCount = 333 * i;
+                }
+            }
+            
+        }
+        // add circles to the world
+        if (counter == 0){
+            for (int i=0; i<4; i++){
+                addObject(population[i], population[i].getX(), population[i].getY());
+                addObject(masked[i], masked[i].getX(), masked[i].getY());
+                addObject(patient_zeros[i], patient_zeros[i].getX(), patient_zeros[i].getY());
+                addObject(vaccinated[i], vaccinated[i].getX(), vaccinated[i].getY());
+            }
+            counter++;
         }
     }
 }
