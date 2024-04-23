45a24ed84cc3d746e9f7c1dce496dbeb2e37f279
iUwUc <langchen.felix@gmail.com>
2024-04-23 11:34:28 -0400

    yiuhiyuyub

--- Art.java
+++ Art.java
@@ -8,46 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Art extends Actor
 {
-    protected  Visitor[] visitors;
-    private SpotManager.Spot[] spots;
-    private boolean isNew = false;
-    protected int actNumber;
-    private int len;
-    protected int t=0;//transparency
+    private static final String ART_IMAGE = "art.png";
     /**
      * Act - do whatever the Art wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Art(SpotManager.Spot[] spots){
-        this.spots = spots;
-        len=spots.length;
-        visitors = new Visitor[len];
-        actNumber = 0;
-        visitors=new Visitor[spots.length];
+    public Art(){
+        setImage(ART_IMAGE);
     }
     
     public void addedToWorld(World w){
-        if(!isNew ){
-            isNew = true;
-            SpotManager.addGame(this);
-        }
+        
     }
 
     public void act(){
     
     }
     
-    public Visitor[] getVisitors(){
-        return visitors;
-    }
-    
-    
-    public SpotManager.Spot[] getSpots(){
-        return spots;
-    }
-    
-    public void placeVisitors(Visitor v, int spotNumber){
-        visitors[spotNumber] = v;
-    }
     
 }
