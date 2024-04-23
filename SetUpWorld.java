2e15a98ce4cfdd4c05971c53f9d414270d386642
Jean <111933900+Jeanpjy@users.noreply.github.com>
2024-04-23 14:32:26 -0400

    Added & Improved Classes
    Added the Option Button, Plus Button, and Minus Button classes
    Set the lower and upper bounds for Option Button
    Added functions for Option Button, Plus Button, and Minus Button classes
    Added buttons to the Set Up World

--- SetUpWorld.java
+++ SetUpWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class SetUpWorld here.
  * 
- * @author (your name) 
+ * @author Jerry, Jean Pan
  * @version (a version number or a date)
  */
 public class SetUpWorld extends World
@@ -12,6 +12,12 @@ public class SetUpWorld extends World
     private Color black = Color.BLACK;
     private Font boringFont = new Font ("Joystix", false, false, 18);
     private HomeButton homeButton;
+    
+    //Set the buttons for guards, robbers, and valuables
+    private OptionButton guardButton;
+    private OptionButton robberButton;
+    private OptionButton valuableButton;
+    
     /**
      * Constructor for objects of class SetUpWorld.
      * 
@@ -33,5 +39,38 @@ public class SetUpWorld extends World
         homeButton.setScale(130, 75);
         addObject(homeButton, 75, 50);
         Button.init();
+        
+        
+        
+        //Set the buttons for guards, robbers, and valuables
+        guardButton = new OptionButton("Number of Guards in World: ", 1, 10);
+        robberButton = new OptionButton("Number of Robbers in World: ", 1, 10);
+        valuableButton = new OptionButton("Number of Valuables in World: ", 5, 20);
+        
+        //Add these buttons onto the world
+        addObject(guardButton, getWidth()/2, getHeight()/8*3);
+        addObject(robberButton, getWidth()/2, getHeight()/8*4);
+        addObject(valuableButton, getWidth()/2, getHeight()/8*5);
+    }
+    
+    /**
+     * Return the number of guards set by the user.
+     */
+    public int getNumberOfGuards(){
+        return guardButton.getValue();
+    }
+    
+    /**
+     * Return the number of robbers set by the user.
+     */
+    public int getNumberOfRobbers(){
+        return robberButton.getValue();
+    }
+    
+    /**
+     * Return the number of valuables set by the user.
+     */
+    public int getNumberOfValuables(){
+        return valuableButton.getValue();
     }
 }
